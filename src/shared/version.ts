4d0da09ad1a72e3a0884845ff095b8f6ba81f974
Alex Nomad <249350008+RsNomad@users.noreply.github.com>
2026-09-06 00:30:58 +0700

    chore: bump version to 0.2.0 for v0.2.0-beta.1 release

--- src/shared/version.ts
+++ src/shared/version.ts
@@ -8,4 +8,4 @@
  */
 export const EXTENSION_NAME = 'talaria-code';
 export const EXTENSION_TITLE = 'Talaria Code';
-export const EXTENSION_VERSION = '0.1.5';
+export const EXTENSION_VERSION = '0.2.0';
